86f8c881795492cc84b58a468e0214864df08ab0
prigozhinzhenya2007 <jey123sun456@mail.ru>
2025-11-07 19:31:04 +0300

    Update 4.py

--- lab4/4.py
+++ lab4/4.py
@@ -1,1 +1,13 @@
+import math
+import datetime
+import my_module
 
+#1
+num = int(input())
+print(math.sqrt(num))
+print(datetime.datetime.now())
+
+#2, 3
+n1 = int(input())
+n2 = int(input())
+print(my_module.plus(n1, n2), my_module.minus(n1, n2), my_module.multiply(n1, n2), my_module.divide(n1, n2))
